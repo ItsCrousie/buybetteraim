dbbb4ccc0ea7d806a53f5dc073509a15d370ea93
ItsCrousie <itscrousie@gmail.com>
2023-05-03 19:38:01 -0400

    Fix: Prep images for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
   const domain = 'buybetteraim.vercel.app'
-  // const domain : string = 'localhost'
+  // const domain : string = 'localhost' //for dev env
   return (
     <div className="app">
       <ul className="links">
@@ -12,7 +12,7 @@ function App() {
         <li><a href="https://twitter.com/TheSlavicCat">Twitter</a></li>
         <li><a href="https://www.tiktok.com/@slavicttv">TikTok</a></li>
       </ul>
-      <img src="public/slavic_header.png" alt="The Slavic Cat" className="app-header" loading='lazy'/>
+      <img src="https://static-cdn.jtvnw.net/jtv_user_pictures/9d14f960-15e2-4acc-ab58-fba7372da309-profile_banner-480.png" alt="The Slavic Cat" className="app-header" loading='lazy'/>
       <iframe
           className='liveStream'
           src={`https://player.twitch.tv/?channel=slavic&parent=${domain}`}

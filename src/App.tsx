e5daf764359a0c3703acc18758acb0b1aeb6fc55
ItsCrousie <itscrousie@gmail.com>
2023-05-03 15:53:50 -0400

    Feat: Embed stream, add socials and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import './App.css'
 
-function App() {
 
+function App() {
+  // const domain = 'http://www.buybetteraim.com'
+  const domain : string = 'localhost'
   return (
-    <>
-      {/* add twitch frame, socials, etc */}
-    </>
+    <div className="app">
+      <ul className="links">
+        <li><a href="https://www.youtube.com/channel/UCy7XlF1DcGzgbZdCqUYR-PQ?view_as=subscriber">Youtube</a></li>
+        <li><a href="https://www.instagram.com/theslaviccatttv/?hl=en">Instagram</a></li>
+        <li><a href="https://twitter.com/TheSlavicCat">Twitter</a></li>
+        <li><a href="https://www.tiktok.com/@slavicttv">TikTok</a></li>
+      </ul>
+      <img src="/src/assets/slavic_header.png" alt="The Slavic Cat" className="app-header" loading='lazy'/>
+      <iframe
+          className='liveStream'
+          src={`https://player.twitch.tv/?channel=slavic&parent=${domain}`}
+          width="1080px"
+          height="720px"
+          allowFullScreen
+        ></iframe>
+    </div>
   )
 }
 
